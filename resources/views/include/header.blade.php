71e635e8cc38ecff4b703308defabb6b7df29cf1
ilhamarrafat <ilhamarrafat@gmail.com>
2024-06-02 13:31:00 +0700

    t.dashboard

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,25 @@
+{{--header--}}
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="{{asset('template/assets/img/logo.png')}}" alt="">
+        <h1 class="sitename">
+          Pondok Pesantren Manba'ul Anwar
+        </h1>
+        
+      </a>
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="{{asset('template/hero')}}" class="">Home</a></li>
+          <li><a href="{{asset('template/about')}}">Berita</a></li>
+          <li><a href="{{asset('template/services')}}">Kontak</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+      <a class="btn-getstarted" href="#about">Login</a>
+
+    </div>
+  </header>
---
+++ resources/views/include/script.blade.php
@@ -0,0 +1,13 @@
+{{--javasript--}}
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('template/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src="{{asset('template/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <!-- Main JS File -->
+  <script src="{{asset('template/assets/js/main.js')}}"></script>
